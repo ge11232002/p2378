72bb0598d0d4514d4c5bc427c7506f064261ecb3
Ge Tan <gtan@me.com>
2021-10-30 08:21:30 +0200

    add GEO

---
+++ Code/RNA-seq/prepGEO.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+dataset <- read_tsv("/Volumes/Transcend/p2378/dataset.tsv") %>%
+  filter(str_detect(Name, "24hrs")) %>%
+  mutate("R1" = str_replace(`Read1 [File]`, fixed("p2378/NovaSeq_20211020_NOV982_o25922_DataDelivery/"), "")) %>%
+  select(Name, R1)
+
+md5sum <- read_delim("/Volumes/Transcend/p2378/md5sums.txt", col_names = FALSE)
+ans <- dataset %>% left_join(md5sum, by = c("R1"="X3"))
+write_tsv(ans, "GEO.txt")
+
+
